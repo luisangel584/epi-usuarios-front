c2dcf1162f0060885e857806066d5c26e44fd7d2
luiscouoh <luis_angel584@hotmail.com>
2026-08-13 13:54:42 -0500

    testing(app, users): fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { rolesReducer } from './roles/store/roles.reducer';
+import { usersReducer } from './users/store/users.reducer';
+import { httpLoadingReducer } from './http-loading/store/http-loading.reducer';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideRouter([]),
+        provideStore({
+          roles: rolesReducer,
+          users: usersReducer,
+          httpLoading: httpLoadingReducer,
+        }),
+      ],
     }).compileComponents();
   });
 
@@ -20,10 +33,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('interview');
   });
 
-  it('should render title', () => {
+  it('should show the boot loading screen while the current user has not loaded', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, interview');
+    expect(compiled.querySelector('.app-boot-loading')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeFalsy();
   });
 });
